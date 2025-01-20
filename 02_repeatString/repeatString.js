26a249c5643a9cd62daaec21d09379f3bd3038d4
Assad Ben Brahim <assad.ben.brahiim@gmail.com>
2025-01-20 13:02:50 +0100

    Completed 02_REPEATSTRING

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(text,x) {
+    if(x < 0) return "ERROR";
+    let string = ""
+    for(let i = 0; i < x; i++) {
+        string += text;
+    }
 
+    return string;
 };
 
 // Do not edit below this line
