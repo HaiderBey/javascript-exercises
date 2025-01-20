6da8b529ccad411fcb855dcc6aedbe71f9949e48
Assad Ben Brahim <assad.ben.brahiim@gmail.com>
2025-01-20 19:43:03 +0100

    Completed 05_SUMALL

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    
+    if(!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if(start < 0 || end < 0) return "ERROR";
+    
+    if(start > end) {
+        const aux = start;
+        start = end;
+        end = aux;
+    }
+
+    let result = 0;
+    for(;start <= end; start++) {
+        result += start;
+    }
+
+    return result;
 
 };
 
