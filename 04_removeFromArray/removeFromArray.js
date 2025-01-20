da1314e978e23d0b06e9ded3c55da16797e8b1b9
Assad Ben Brahim <assad.ben.brahiim@gmail.com>
2025-01-20 17:49:40 +0100

    Completed 04_REMOVEFROMARRAY

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    for (let e of elements){
+        array = array.filter(item => item !== e);
+    }
+    // for (let e of elements){
+    //     while(array.indexOf(e) != -1) {
+    //         array.splice(array.indexOf(e),1);
+    //     }
+    // }
+
+    return array;
 };
 
 // Do not edit below this line
